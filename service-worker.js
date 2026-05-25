4166d679675e453a33cf4a98e06c96b51205606c
beNOWn <dzozefstar@gmail.com>
2026-05-25 22:37:02 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'church-schedule-v1';
+const CACHE_NAME = 'raspisanie-sluzhb-v1';
+
 const urlsToCache = [
   './',
   'index.html',
@@ -14,13 +15,18 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
